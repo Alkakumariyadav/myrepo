619fa6edc2cd66892911d358fd44162586c1358e
HP <HP@Alka-Laptop>
2024-08-17 13:13:21 +0530

    commit 1

---
+++ src/com/tnsif/day1/IfDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.day1;
+
+public class IfDemo {
+	public static void main(String[] args) {
+		int x = 4;
+		if(x != 5) {
+			System.out.println("value of x is not 5");
+		}
+		else {
+			System.out.println("value of x is 5");
+		}
+	}
+
+}
---
+++ src/com/tnsif/day2/c43_Alkla/DoWhileDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.day2.c43_Alkla;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 11;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 1);
+	}
+
+
+	}
+
+
